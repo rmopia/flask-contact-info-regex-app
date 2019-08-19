5d1f01e500f16245114862b247bbc8feba490cac
Robert Mopia <53323390+rmopia@users.noreply.github.com>
2019-08-19 11:16:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
-from forms import UploadForm
+from flask import Flask, render_template, request, redirect, url_for, flash
 import re
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = # generate your own secret key
+app.config['SECRET_KEY'] = '2090cdcebc9658f5eefd810a80096273'
 
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -25,23 +24,29 @@ def home():
 @app.route('/', methods=['POST'])
 @app.route('/home', methods=['POST'])
 def home_regex():
-    text_block = request.form['text']
-    contacts = regex_find(text_block)
-    results = 'Results:<br/>' + '<br/>'.join(contacts)
-    return results
+    text_area = request.form['text']
+    if text_area is '':
+        flash('The text box below is empty! Please fill it in.')
+        return redirect(url_for('home'))
+    else:
+        phone_nums = regex_find_phone_num(text_area)
+        emails = regex_find_email(text_area)
+        return render_template('results.html', phone_nums=phone_nums, emails=emails, title='Results')
 
 
-def regex_find(text_block):
-    result = ['Numbers:']
+def regex_find_phone_num(text_block):
+    result = []
     phone_regex = re.compile(r'(((\(\d\d\d\))|(\d\d\d))?(\s|-)(\d\d\d)(-)(\d\d\d\d))')
-    email_regex = re.compile(r'[a-zA-Z0-9._+"-]+@[a-zA-Z0-9]+\.[a-zA-Z]+')
     result_phone = phone_regex.findall(text_block)
-    result_email = email_regex.findall(text_block)
-    result_email.insert(0, 'Emails:')
     for item in result_phone:
         result.append(item[0].strip(' '))
-    result = result + result_email
     return result
+
+
+def regex_find_email(text_block):
+    email_regex = re.compile(r'[a-zA-Z0-9._+"-]+@[a-zA-Z0-9]+\.[a-zA-Z]+')
+    result_email = email_regex.findall(text_block)
+    return result_email
 
 
 if __name__ == '__main__':
